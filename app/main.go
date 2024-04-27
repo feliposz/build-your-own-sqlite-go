68032966bf790a4163bae5acaf8732de54cc0025
feliposz <feliposz@gmail.com>
2024-04-27 05:22:34 -0300

    Better parsing of column and constraint definitions

--- app/main.go
+++ app/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"slices"
 	"strings"
+	"unicode"
 	"unicode/utf16"
 )
 
@@ -280,7 +281,6 @@ func (db *DbContext) readSchema() {
 			db.Info.NumberOfTriggers++
 		case "view":
 			db.Info.NumberOfViews++
-			entry.Columns, entry.Constraints = parseColumns(entry.SQL)
 		case "index":
 			db.Info.NumberOfIndexes++
 			entry.Columns, _ = parseColumns(entry.SQL)
@@ -304,28 +304,141 @@ func parseColumns(sql string) (columns []ColumnDef, constraints []string) {
 	if leftParen < 0 || rightParen < 0 {
 		return
 	}
-	// remove everything before first "(" and last ")"
+	// remove everything before first "(" and after last ")"
 	sql = sql[leftParen+1 : rightParen]
-	// TODO: type names can have "," inside them! parse this properly!!!
-	// TODO: handle quoted names
-	for _, columnDefinition := range strings.Split(sql, ",") {
-		columnDefinition = strings.TrimSpace(columnDefinition)
-		parts := strings.Split(columnDefinition, " ")
-		switch strings.ToUpper(parts[0]) {
-		case "PRIMARY", "CONSTRAINT", "UNIQUE", "CHECK", "FOREIGN":
-			constraints = append(constraints, columnDefinition)
-		default:
-			column := ColumnDef{}
-			column.Name = parts[0]
-			if len(parts) > 1 {
-				column.Type = parts[1]
+
+	// tokenize column definitions for processing
+	r := strings.NewReader(sql)
+	tokens := []string{}
+	for {
+		ch, _, err := r.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
 			}
-			if len(parts) > 2 {
-				column.Constraints = parts[2:]
+			panic(err)
+		}
+		if unicode.IsSpace(ch) {
+			continue
+		}
+
+		if ch == '"' {
+			// quotes are not part of the token
+			runes := []rune{}
+			for {
+				ch, _, err := r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				if ch == '"' {
+					break
+				}
+				runes = append(runes, ch)
+			}
+			tokens = append(tokens, string(runes))
+		} else if ch == '[' {
+			// brackets are not part of the token
+			runes := []rune{}
+			for {
+				ch, _, err := r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				if ch == ']' {
+					break
+				}
+				runes = append(runes, ch)
+			}
+			tokens = append(tokens, string(runes))
+		} else if ch == '(' {
+			// parenthesis ARE part of the token
+			runes := []rune{'('}
+			for {
+				ch, _, err := r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				runes = append(runes, ch)
+				if ch == ')' {
+					break
+				}
+			}
+			tokens = append(tokens, string(runes))
+		} else if ch == ',' {
+			tokens = append(tokens, ",")
+		} else {
+			runes := []rune{ch}
+			for {
+				ch, _, err := r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				if ch == ',' {
+					r.UnreadRune()
+					break
+				}
+				if unicode.IsSpace(ch) {
+					break
+				}
+				runes = append(runes, ch)
+			}
+			tokens = append(tokens, string(runes))
+		}
+	}
+
+	if debugMode {
+		fmt.Printf("%#v\n", tokens)
+	}
+
+	for i := 0; i < len(tokens); i++ {
+		switch strings.ToUpper(tokens[i]) {
+		case "PRIMARY", "CONSTRAINT", "UNIQUE", "CHECK", "FOREIGN":
+			// table constraints start with one of these keywords and everything is part of the definition until the next ","
+			for i < len(tokens) && tokens[i] != "," {
+				constraints = append(constraints, tokens[i])
+				i++
+			}
+		default:
+			// column definitions always start with column name
+			column := ColumnDef{}
+			column.Name = tokens[i]
+			i++
+			// type and constraints are optional
+			typeTokens := []string{}
+			for i < len(tokens) && tokens[i] != "," {
+				switch strings.ToUpper(tokens[i]) {
+				case "PRIMARY", "CONSTRAINT", "UNIQUE", "CHECK", "REFERENCES", "NOT", "NULL", "DEFAULT", "COLLATE", "GENERATED":
+					// column constraints start with one of these keywords and everything else until the "," is constraints
+					for i < len(tokens) && tokens[i] != "," {
+						column.Constraints = append(column.Constraints, tokens[i])
+						i++
+					}
+				default:
+					// everything after the column name and before the constraints are part of the "type name"
+					typeTokens = append(typeTokens, tokens[i])
+					i++
+				}
+			}
+			column.Type = strings.Join(typeTokens, " ")
+			if debugMode {
+				fmt.Printf("%#v\n", column)
 			}
 			columns = append(columns, column)
 		}
 	}
+
 	return
 }
 
