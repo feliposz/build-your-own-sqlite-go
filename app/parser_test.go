c62bc98815692d29355845a74a190aee182e1812
feliposz <feliposz@gmail.com>
2024-04-30 05:25:53 -0300

    TestParseCreateIndex

--- app/parser_test.go
+++ app/parser_test.go
@@ -24,3 +24,23 @@ func TestTokenizer(t *testing.T) {
 		}
 	}
 }
+
+func TestParseCreateIndex(t *testing.T) {
+	indexName, tableName, columns := parseCreateIndex("create index idx on tab (a, b desc, c asc)")
+	if indexName != "idx" {
+		t.Errorf("expected index name: %q - got: %q\n", "idx", indexName)
+	}
+	if tableName != "tab" {
+		t.Errorf("expected table name: %q - got: %q\n", "tab", tableName)
+	}
+	for i, name := range []string{"a", "b", "c"} {
+		if columns[i].Name != name {
+			t.Errorf("expected column name: %q - got: %q\n", name, columns[i].Name)
+		}
+	}
+	for i, order := range []string{"ASC", "DESC", "ASC"} {
+		if columns[i].Type != order {
+			t.Errorf("expected column order: %q - got: %q\n", order, columns[i].Type)
+		}
+	}
+}
